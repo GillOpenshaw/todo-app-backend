b8a229e78965e8d060189c4e264777b1c6035701
GillianOpenshaw <openshaw.gillian@gmail.com>
2019-05-28 20:50:41 +0100

    An attempt at task

--- tasks.js
+++ tasks.js
@@ -4,13 +4,33 @@ const serverless = require("serverless-http");
 const app = express(); // get a new express application
 
 // do we want it to respond to get request, post request etc
+const itinerary = [
+  {
+    "id": 1,
+    "date": 06 / 08 / 2019,
+    "title": "Theatre Tickets",
+    "price": 80,
+    "completed": true
+  },
+
+  {
+    "id": 2,
+    "date": 07 / 08 / 2019,
+    "title": "Hotel",
+    "price": 75,
+    "completed": false
+  }];
 
 app.get("/tasks", function (request, response) {
   console.log(request);
   const username = request.query.username;
   response.json({
-    message: `Username ${username} requested the tasks`
-})
+    itinerary.map((itineraryList, index) => {
+      message: `Username ${username} is booking` 
+      return itineraryList.title
+    })
+
+  })
 }); // when called will receive object about request and object about response
 
 module.exports.handler = serverless(app);
