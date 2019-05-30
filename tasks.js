9853508bfa0ca889c5fd0f179e2b46efe512eaf5
GillianOpenshaw <openshaw.gillian@gmail.com>
2019-05-30 12:06:10 +0100

    Tasks returning via serverless

--- tasks.js
+++ tasks.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const mysql = require("mysql"); // connect to database
 
 const app = express(); // get a new express application
 
-// do we want it to respond to get request, post request etc
-const itinerary = [
-  {
-    "id": 1,
-    "date": "06 / 08 / 2019",
-    "title": "Theatre Tickets",
-    "price": 80,
-    "completed": true
-  },
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, // from RDS
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+}); // add values for this to work from end point
 
-  {
-    "id": 2,
-    "date": "07 / 08 / 2019",
-    "title": "Hotel",
-    "price": 75,
-    "completed": false
-  }];
+// Connect to mysql via command line
+
+// do we want it to respond to get request, post request etc
+
 
 app.get("/tasks", function (request, response) {
-  console.log(request);
-  response.json({
-    tasks: itinerary
-  })
-
+  connection.query("SELECT * FROM Tasks", function (err, result, fields) { //error result fields
+    if (err !== null) { // Response 200 is ok, all good
+      console.log("Error fetching tasks", err);
+      // Respond to end client with suitable response
+      // Use .status then sets status code but not response
+      // Use .send then also sends an error message response
+      response.send(500);
+    }
+    response.json({ tasks: result });  // returns database items to browser. EndPoint
+  });
 }); // when called will receive object about request and object about response
 
 module.exports.handler = serverless(app);
+
+//environment variables can be accessed from the code but are not in the code e.g. for password not to be hardcoded.
